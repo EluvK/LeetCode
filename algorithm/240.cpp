8f3bb6b567ce6d66ab17c34e7d75389c76e21400
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2020-02-13 00:04:07 +0800

    ADD
    2020/02/12

---
+++ algorithm/240.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool search(vector<vector<int>>& matrix,int m,int n,int target){
+        if(m<0||n>=matrix[0].size()) return false;
+        if(matrix[m][n]==target) return true;
+
+        else if(matrix[m][n]<target) return search(matrix,m,n+1,target);
+        else return search(matrix,m-1,n,target);
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        if(m==0) return false;
+        int n=matrix[0].size();
+        return search(matrix,m-1,0,target);
+    }
+};
