5bde1f5c4e62dfb60428d2a98dbe6239d2a8fcf0
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2020-02-15 15:46:06 +0800

    ADD
    2020/02/14

---
+++ algorithm/18.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int len=nums.size();
+        vector<vector<int>> ans;
+        if(len<4) return ans;
+        for(int i=0;i<nums.size()-3;++i){
+            if(i&&nums[i]==nums[i-1]) continue;
+            int t=target-nums[i];
+            for(int j=i+1;j<nums.size()-2;++j){
+                if(j!=i+1&&nums[j]==nums[j-1]) continue;
+                int l=j+1,r=nums.size()-1;
+                while(l<r){
+                    // cout<<nums[i]<<" "<<nums[j]<<" "<<nums[l]<<" "<<nums[r]<<endl;
+                    int sum=nums[j]+nums[l]+nums[r];
+                    if(sum==t){
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        l++;r--;
+                        while(l<len&&nums[l]==nums[l-1]) l++;
+                        while(r>0&&nums[r]==nums[r+1]) r--;
+                    }else if(sum<t){
+                        l++;while(l<len&&nums[l]==nums[l-1]) l++;
+                    }else{
+                        r--;while(r>0&&nums[r]==nums[r+1]) r--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
