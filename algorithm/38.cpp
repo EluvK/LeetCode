e0b64b90588bdcaac7b5a48025484b10fc786f10
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2020-03-24 12:26:00 +0800

    ADD
    2020/03/24

---
+++ algorithm/38.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string read(string s){
+        int i=0,j=0;
+        string ans;
+        while(i<s.size()){
+            while(j<s.size()&&s[j]==s[i]) j++;
+            ans.push_back(j-i+'0');
+            ans.push_back(s[i]);
+            i=j;
+        }
+        return ans;
+    }
+    string countAndSay(int n) {
+        string ans="1";
+        while(--n) ans=read(ans);
+        return ans;
+    }
+};
