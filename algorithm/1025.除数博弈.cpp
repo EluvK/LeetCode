fcc2dc2b5e0e34da19d9c798c9c463519260e729
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2020-07-24 13:40:53 +0800

    code format

---
+++ algorithm/1025.除数博弈.cpp
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=cpp
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+using namespace std;
+class Solution {
+public:
+    bool divisorGame(int N) { return !(N & 1); }
+};
+// @lc code=end
+
