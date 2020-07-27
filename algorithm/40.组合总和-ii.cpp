4e0a8b3f8d96629657a78e42390dc284a72bb8ae
Eucalypt <36977935+EucalyptSAMA@users.noreply.github.com>
2020-07-27 15:11:55 +0800

    code format

---
+++ algorithm/40.组合总和-ii.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=40 lang=cpp
+ *
+ * [40] 组合总和 II
+ */
+
+// @lc code=start
+using namespace std;
+class Solution {
+public:
+    void solve(set<vector<int>> & ans, vector<int> & res, vector<int> & candidates, int sum, int index) {
+        if (sum == 0) {
+            ans.insert(res);
+            return;
+        }
+        if (sum < 0 || index >= candidates.size())
+            return;
+        for (int i = index; i < candidates.size(); ++i) {
+            res.push_back(candidates[i]);
+            solve(ans, res, candidates, sum - candidates[i], i + 1);
+            res.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int> & candidates, int target) {
+        set<vector<int>> ans;
+        vector<int> res;
+        sort(candidates.begin(), candidates.end());
+        solve(ans, res, candidates, target, 0);
+        vector<vector<int>> vans(ans.begin(), ans.end());
+        return vans;
+    }
+};
+// @lc code=end
---
+++ algorithm/42.接雨水.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=cpp
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+using namespace std;
+struct node {
+    int x, h;
+    node(int _x, int _h) {
+        x = _x;
+        h = _h;
+    }
+    bool operator<(const node & other) const { return h > other.h; }
+};
+class Solution {
+public:
+    int trap(vector<int> & height) {
+        int n = height.size();
+        if (n < 3)
+            return 0;
+        priority_queue<node> q;
+        q.push(node{0, height[0]});
+        q.push(node{n - 1, height[n - 1]});
+        vector<bool> vis(n, false);
+        vis[0] = vis[n - 1] = true;
+        int dx[2] = {1, -1};
+        int ans = 0;
+        while (!q.empty()) {
+            node p = q.top();
+            q.pop();
+            for (int i = 0; i < 2; ++i) {
+                int x = p.x + dx[i];
+                if (x < 0 || x >= n)
+                    continue;
+                if (!vis[x]) {
+                    if (height[x] >= p.h) {
+                        q.push(node{x, height[x]});
+                        vis[x] = true;
+                    } else {
+                        ans += p.h - height[x];
+                        q.push(node{x, p.h});
+                        vis[x] = true;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
